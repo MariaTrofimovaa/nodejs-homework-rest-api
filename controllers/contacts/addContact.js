bcb4285a5329b83bff313d85205e4a75bad28e86
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-08-24 13:42:17 +0300

    finished task 4

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,35 @@
+const { contacts: service } = require("../../services");
+
+const addContact = async (req, res, next) => {
+  try {
+    const newContact = { ...req.body, owner: req.user._id };
+    const result = await service.add(newContact);
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// const addContact = async (req, res, next) => {
+//   try {
+//     const result = await service.add(req.body);
+
+//     res.status(201).json({
+//       status: "success",
+//       code: 201,
+//       data: {
+//         result,
+//       },
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+module.exports = addContact;
